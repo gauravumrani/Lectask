0ebcd39b7efe8f4665c6b3037c0c8605acd910fd
gauravumrani <gauravumrani@gmail.com>
2019-04-28 21:46:41 +0530

    setup react and typescript

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Lectask
 Electron based task management application which lets you to organise your tasks. 
+
+
+## Tech Stack
+1. React + Electron + TypeScript
+2. Ant Design
---
+++ src/components/App/index.tsx
@@ -0,0 +1,14 @@
+
+import * as React from 'react';
+import './style.scss';
+
+class App extends React.Component {
+  render() {
+    return (<div>
+      <h1>Welcome to React with Typescript</h1>
+    </div>
+    );
+  }
+}
+
+export default App;
